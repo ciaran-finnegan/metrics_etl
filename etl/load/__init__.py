7dd8c41d5d2cc9604b871d6bcc573e3c0b672d9a
Ciaran Finnegan <6890923+ciaran-finnegan@users.noreply.github.com>
2025-04-21 10:49:02 +0800

    fix(load): expose loader classes in etl/load package init

--- etl/load/__init__.py
+++ etl/load/__init__.py
@@ -1,1 +1,5 @@
 # This file makes the 'etl/load' directory a Python package. 
+
+from .base_loader import BaseLoader
+from .google_sheets_loader import GoogleSheetsLoader
+from .supabase_loader import SupabaseLoader 
